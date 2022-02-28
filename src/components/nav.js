6cd5783ba1ecb9aa0fbc7361e55990f87b9fc4f6
maxmcgrath1 <tuc06096@temple.edu>
2022-02-28 15:35:40 -0500

    moved files to pages, nav BP

---
+++ src/components/nav.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Nav = (props) => {
+    return (
+        <div className="nav">
+            <Link to="/">
+                <div>HOME</div>
+            </Link>
+            <Link to="/price">
+                <div>CRYPTO PRICES</div>
+            </Link>
+            <Link to="/currencies">
+                <div>CURRENCIES</div>
+            </Link>
+        </div>
+    );
+};
+
+export default Nav;
